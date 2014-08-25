d14e889327cc8dd90764372a0a6da1bc9fd98daa
Young-Mook Kang <youngmook@opengsi.org>
2014-08-25 22:15:24 +0900

    Add org.opengsi.math.GsiVector

---
+++ src/main/java/org/opengsi/math/GsiVector.java
@@ -0,0 +1,18 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (C) 2014, Young-Mook Kang <youngmook@opengsi.org>.
+ */
+
+package org.opengsi.math;
+
+/**
+ *
+ * @author Young-Mook Kang <youngmook@opengsi.org>
+ * @since  v0.1
+ */
+public class GsiVector {
+
+}
